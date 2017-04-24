29ca10b255b5e03900cbaa0c6aa3568c8bfdb62b
himanshu <himanshu.yadav@sendinblue.com>
2017-04-25 01:25:38 +0530

    a

--- examples/messenger.js
+++ examples/messenger.js
@@ -191,53 +191,54 @@ app.get('/zalo', (req, res) => {
             resulte.forEach(function (resulte,iop){
                 console.log(resulte)
                 console.log(iop)
-            });
+            
 
 
-            console.log(err)
+            //console.log(err)
 
-            //console.log(resulte);
+            console.log(resulte);
 
-            // var result = resulte;
+            var result = resulte;
 
-            // var name = result.name;
-            // var phone = result.phone;
-            // var oaid = '1032900368143269705';
-            // var company = result.company;
-            // var number = result.number;
-            // var date = result.date;
-            // var templateid = result.templateid;
-            // var timestamp = new Date().getTime();
-            // var secretkey = 'IEklE4N1I7bWqp5TOQ2F';
+            var name = result.name;
+            var phone = result.phone;
+            var oaid = '1032900368143269705';
+            var company = result.company;
+            var number = result.number;
+            var date = result.date;
+            var templateid = result.templateid;
+            var timestamp = new Date().getTime();
+            var secretkey = 'IEklE4N1I7bWqp5TOQ2F';
 
-            // var data = '{"phone":'+phone+',"templateid":'+templateid+',"templatedata":{"name":'+name+',"company":'+company+',"number":'+number+',"date":'+date+'}}';
+            var data = '{"phone":'+phone+',"templateid":'+templateid+',"templatedata":{"name":'+name+',"company":'+company+',"number":'+number+',"date":'+date+'}}';
 
-            // execPhp('messenger.php', (error, php, outprint) => { 
+            execPhp('messenger.php', (error, php, outprint) => { 
 
-            //     php.my_function(oaid, data, timestamp, secretkey, (err, results, output, printed) => {
+                php.my_function(oaid, data, timestamp, secretkey, (err, results, output, printed) => {
 
-            //         console.log(results)
+                    console.log(results)
                
-            //         var options = { method: 'POST',
-            //             url: 'https://openapi.zaloapp.com/oa/v1/sendmessage/phone/cs',
-            //             qs: { 
-            //                 oaid: oaid,
-            //                 data: data,
-            //                 timestamp: '1492597209077',
-            //                 mac: results
-            //             },
-            //             headers: { 
-            //                 'cache-control': 'no-cache' 
-            //             } 
-            //         };
+                    var options = { method: 'POST',
+                        url: 'https://openapi.zaloapp.com/oa/v1/sendmessage/phone/cs',
+                        qs: { 
+                            oaid: oaid,
+                            data: data,
+                            timestamp: '1492597209077',
+                            mac: results
+                        },
+                        headers: { 
+                            'cache-control': 'no-cache' 
+                        } 
+                    };
 
-            //         request(options, function (error, response, body) {
-            //           if (error) throw new Error(error);
+                    request(options, function (error, response, body) {
+                      if (error) throw new Error(error);
 
-            //           console.log(body);
-            //         });
-            //     });
-            // });
+                      console.log(body);
+                    });
+                });
+            });
+            });
             });
         });
 
