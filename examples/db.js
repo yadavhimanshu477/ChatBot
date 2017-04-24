d53177c48332535a7175beff5099b35dcc753e2b
himanshu <himanshu.yadav@sendinblue.com>
2017-04-24 22:31:47 +0530

    a

---
+++ examples/db.js
@@ -0,0 +1,10 @@
+//var MongoClient = require('mongodb').MongoClient
+
+module.exports = {
+	getConnection :function(callback) {
+		MongoClient.connect('mongodb://localhost:27017/Test_shztch', function (err, db) {
+			if (err) throw err
+			callback(db)
+		})
+	},
+}
