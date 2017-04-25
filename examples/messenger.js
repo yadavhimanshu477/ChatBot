4ad50018fc2ba566c56234e6f1fb369e6613d6e8
himanshu <himanshu.yadav@sendinblue.com>
2017-04-25 23:01:09 +0530

    a

--- examples/messenger.js
+++ examples/messenger.js
@@ -187,7 +187,7 @@ app.get('/zalo', (req, res) => {
                     var number = result.number;
                     var date = result.date;
                     var templateid = result.templateid;
-                    var timestamp = new Date().getTime();
+                    var timestamp = 1111111;
                     var secretkey = 'IEklE4N1I7bWqp5TOQ2F';
                     console.log("wwwwwwwwwwwwwww ::::: "+timestamp)
                     //timestamp = 1493140632439;
