b7d828c88785f3067056e86b7c0b3397c366639c
himanshu <himanshu.yadav@sendinblue.com>
2017-04-26 23:23:43 +0530

    after add function zaloMessageFunction

--- examples/zaloMessegeFunction.js
+++ examples/zaloMessegeFunction.js
@@ -8,7 +8,7 @@ module.exports = {
         	db.collection('zalo_contacts', function(err, collection) {
             	collection.find({"uid":uid}).toArray(function(err, resulte) {
             		resulte.forEach(function (resulte,iop){
-            			var string_result = "YourDueDateis"
+            			var string_result = "Your Due Date is"
             			console.log(resulte);
             			callback(string_result);
             		});
