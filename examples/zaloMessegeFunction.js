d3706bd92cbb5488cb04bedc69cfb7a81a361fbf
himanshu <himanshu.yadav@sendinblue.com>
2017-04-26 22:41:44 +0530

    after add function zaloMessageFunction

---
+++ examples/zaloMessegeFunction.js
@@ -0,0 +1,17 @@
+module.exports = {
+	getDueDate :function (uid, callback) {
+		db.getConnection (function (db) {
+        	console.log("connected db from zalomessageFunction page : ")
+
+        	db.collection('zalo_contacts', function(err, collection) {
+            	collection.find({"uid":uid}).toArray(function(err, resulte) {
+            		resulte.forEach(function (resulte,iop){
+            			var string_result = "Your Due Date is "+resulte.date;
+            			console.log(resulte);
+            			callback(string_result);
+            		});
+            	});
+            });
+        });
+	},
+}
